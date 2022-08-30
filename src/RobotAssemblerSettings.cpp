a6f3597d7a1168bcc003c9b42d1bec0157ed90ca
YoheiKakiuchi <kakiuchi.yohei.sw@tut.jp>
2022-08-30 14:53:12 +0900

    fix converting limit,vlimit using UNIT

--- src/RobotAssemblerSettings.cpp
+++ src/RobotAssemblerSettings.cpp
@@ -998,14 +998,24 @@ bool Settings::Impl::parseActuator(ValueNode *vn, Actuator &act)
     std::vector<double> qlim;
     mapVector(mp, "limit", qlim, std::cerr, false);
     if (qlim.size() >= 2) {
-        act.limit[0] = qlim[0];
-        act.limit[1] = qlim[1];
+        if (tp_ ==  ConnectingPoint::Rotational) {
+            act.limit[0] = uc.convAngle(qlim[0]);
+            act.limit[1] = uc.convAngle(qlim[1]);
+        } else {
+            act.limit[0] = qlim[0];
+            act.limit[1] = qlim[1];
+        }
     }
     std::vector<double> vlim;
     mapVector(mp, "vlimit", vlim, std::cerr, false);
     if (vlim.size() >= 2) {
-        act.vlimit[0] = vlim[0];
-        act.vlimit[1] = vlim[1];
+        if (tp_ ==  ConnectingPoint::Rotational) {
+            act.vlimit[0] = uc.convAngle(vlim[0]);
+            act.vlimit[1] = uc.convAngle(vlim[1]);
+        } else {
+            act.vlimit[0] = vlim[0];
+            act.vlimit[1] = vlim[1];
+        }
     }
     std::vector<double> tlim;
     mapVector(mp, "tqlimit", tlim, std::cerr, false);
