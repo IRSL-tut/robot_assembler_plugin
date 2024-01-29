a9078a584ac0d12b0ef9b29024763da307f03937
Yohei Kakiuchi <kakiuchi.yohei.sw@tut.jp>
2024-01-29 18:58:49 +0900

    UPDATE: python-binding

--- pybind11/PyRobotAssembler.cpp
+++ pybind11/PyRobotAssembler.cpp
@@ -6,6 +6,7 @@
 
 #include "../src/irsl_choreonoid/Coordinates.h"
 #include "../src/RobotAssembler.h"
+#include "../src/RobotAssemblerInfo.h"
 
 using namespace cnoid;
 namespace ra = cnoid::robot_assembler;
@@ -45,8 +46,27 @@ void exportPyRobotAssembler(py::module &m)
     .def_property_readonly("settings", &ra::RoboasmUtil::settings)
     .def("makeParts", (ra::RoboasmPartsPtr (ra::RoboasmUtil::*)(const std::string &_ky)) &ra::RoboasmUtil::makeParts)
     .def("makeParts", (ra::RoboasmPartsPtr (ra::RoboasmUtil::*)(const std::string &_ky, const std::string &_nm)) &ra::RoboasmUtil::makeParts)
-    //.def("makeRobot"
-    //.def("makeRobotFromFile"
+    //makeRobotFromKey
+    //makeRobotFromHistory
+    .def("makeRobot", (ra::RoboasmRobotPtr (ra::RoboasmUtil::*)(ra::RoboasmFile &_rafile)) &ra::RoboasmUtil::makeRobot)
+    .def("makeRobotFromFile", &ra::RoboasmUtil::makeRobotFromFile)
+    ;
+
+    //
+    // cnoidRAFile
+    //
+    py::class_< ra::cnoidRAFile > rafile_cls(m, "cnoidRAFile");
+    rafile_cls.def(py::init<const std::string &>())//
+    .def("isValid", &ra::cnoidRAFile::isValid)
+    .def("parseRoboasm", [](ra::cnoidRAFile &self, const std::string &filename, bool parse_config) {
+        return self.parseRoboasm(filename, parse_config);
+    }, py::arg("filename"), py::arg("parseConfig") = true)
+    .def("dumpRoboasm", &ra::cnoidRAFile::dumpRoboasm)
+    .def("historyToMap", &ra::cnoidRAFile::historyToMap, py::arg("map") = nullptr)
+    .def("addInfo", &ra::cnoidRAFile::historyToMap, py::arg("map") = nullptr)
+    .def("updateRobotByInfo", &ra::cnoidRAFile::updateRobotByInfo)
+    .def("makeRobot", &ra::cnoidRAFile::makeRobot,
+         py::arg("util"), py::arg("name") = std::string(), py::arg("rename") = false)
     ;
 }
 
