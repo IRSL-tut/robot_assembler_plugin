eb0a57547c17ed9d878158c18987b3c68cb33e96
Yohei Kakiuchi <kakiuchi.yohei.sw@tut.jp>
2025-04-14 12:29:30 +0900

    [pybind11] add some implements to python-bindings

--- pybind11/PyRobotAssemblerHelper.cpp
+++ pybind11/PyRobotAssemblerHelper.cpp
@@ -9,11 +9,78 @@
 #include "../src/RobotAssemblerBody.h"
 #include "../src/RobotAssemblerInfo.h"
 
+#include <exception>
+
 using namespace cnoid;
 namespace ra = cnoid::robot_assembler;
 namespace py = pybind11;
 
 namespace cnoid {
+
+bool isVariable(const std::string &str)
+{
+    if(str.size() <= 6) {
+        return false;
+    }
+    int n = str.size();
+    if (str[0] != '$') {
+        return false;
+    }
+    if (str[1] != '$') {
+        return false;
+    }
+    if (str[2] != '<') {
+        return false;
+    }
+    if (str[n-3] != '>') {
+        return false;
+    }
+    if (str[n-2] != '$') {
+        return false;
+    }
+    if (str[n-1] != '$') {
+        return false;
+    }
+    return true;
+}
+ValueNode *deepCopy(ValueNode *nv) {
+    if(nv->isValid()) {
+        if(nv->isScalar()) {
+            if (nv->isString()) {
+                ValueNode *res = new ScalarNode("$$<str>$$");
+                return res;
+            }
+            return nv->clone();
+        }
+        if(nv->isListing()) {
+            ValueNode *res = new Listing();
+            Listing *l = nv->toListing();
+            Listing *dst = res->toListing();
+            for(auto it = l->begin(); it != l->end(); it++) {
+                dst->append(deepCopy(*it));
+            }
+            return res;
+        }
+        if(nv->isMapping()) {
+            ValueNode *res = new Mapping();
+            Mapping *m = nv->toMapping();
+            Mapping *dst = res->toMapping();
+            for(auto it = m->begin(); it != m->end(); it++) {
+                // it->first // key
+                // it->second // value
+                std::string key(it->first);
+                key += "$$<key>$$";
+                dst->insert(key, deepCopy(it->second));
+            }
+            return res;
+        }
+        // invalid
+        std::cout << "invalid 1" << std::endl;
+    }
+    // invalid
+    std::cout << "invalid 2" << std::endl;
+    return nullptr;
+};
 
 void exportPyRobotAssemblerHelper(py::module &m)
 {
@@ -66,6 +133,22 @@ void exportPyRobotAssemblerHelper(py::module &m)
     m.def("addRootOffset", [](Body *bd, const coordinates &cds) {
         Isometry3 T; cds.toPosition(T);
         return ra::addRootOffset(bd, T);
+    });
+    m.def("testYaml", [](std::string fname) {
+        //ValueNode *res = new ValueNode();
+        YAMLReader yaml_reader;
+        if (! yaml_reader.load(fname)) {
+            std::cerr << "File Loading error : " << fname << std::endl;
+            throw std::exception();
+        }
+        for(int i = 0; i < yaml_reader.numDocuments(); i++) {
+            ValueNode *val = yaml_reader.document(i);
+            if (val->isValid()) {
+                ////
+                return deepCopy(val);
+            }
+        }
+        throw std::exception();
     });
 
 }
